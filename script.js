ada051e84c0b55ccfb5b2de976d0c435cc0826fc
robtavares <rctavares@rogers.com>
2017-11-20 20:49:05 -0500

    Completed dashboard project.

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
-  /* Select a Subject */
+  /* Select a Subject - Sample Interactive Element */
   $('#subject-science-choose').click(function() {
     $('#subject-science-selected').fadeIn();
      $('#subject-science-choose').hide();
   });
 
-  /* Unselect the Subject */
+  /* Unselect the Subject - Sample Interactive Element */
   $('#subject-science-selected').click(function() {
     $('#subject-science-choose').fadeIn();
      $('#subject-science-selected').hide();
